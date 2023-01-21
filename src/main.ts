0e29e4dda1f682fa95adb251548f6793f38e4492
KombinatorVit <slonik0775@gmail.com>
2023-01-21 20:40:43 +0200

    Создание моделей, реализация валидации входящих данных Data Transfer Object DTO

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "@nestjs/common";
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+  app.useGlobalPipes(new ValidationPipe())
+
   const config = new DocumentBuilder()
       .setTitle("Lesson api")
       .setDescription("This api for lesson")
